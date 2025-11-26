484934299dcdd0ace7f0edeb14ad017274d45a93
Pratchaya Leelanuwat <67160017@go.buu.ac.th>
2025-11-26 13:46:32 +0700

    Refactor: Separate HTML, CSS, and JavaScript

---
+++ scritp.js
@@ -0,0 +1,4 @@
+cument.getElementById("btn-show").addEventListener("click", function () {
+ const msg = document.getElementById("message");
+ msg.style.display = msg.style.display === "none" ? "block" : "none";
+});
