f981350098979c0d959a02acc7fe5372e4d74a63
Pratchaya-Leelanuwat <67160017@go.buu.ac.th>
2025-11-26 14:17:56 +0700

    Fix code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-cument.getElementById("btn-show").addEventListener("click", function () {
+cument.getElementById("btn").addEventListener("click", function () {
  const msg = document.getElementById("message");
  msg.style.display = msg.style.display === "none" ? "block" : "none";
 });
